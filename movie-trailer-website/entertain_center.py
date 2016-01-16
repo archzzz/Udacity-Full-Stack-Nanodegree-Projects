af46347692b1c9c8ce2a03b1061dc9b0e57005d0
archzzz <zhangaq2014@gmail.com>
2016-01-16 17:46:53 -0600

    correct bugs in finding images

--- movie-trailer-website/entertain_center.py
+++ movie-trailer-website/entertain_center.py
@@ -4,8 +4,8 @@ import csv
 from media import Movie
 
 # read movie informations from movies_info.csv, and store in a list movies
-with open('movies_info.csv', 'rb') as csvfiles:
-	movies = map(lambda entry:Movie.make_movie(entry), csv.reader(csvfiles, delimiter=","))
+with open('db/movies_info.csv', 'rb') as csvfiles:
+    movies = map(lambda entry: Movie.make_movie(
+        entry), csv.reader(csvfiles, delimiter=","))
 
-# 
 fresh_tomato.open_movies_page(movies)
--- movie-trailer-website/media.py
+++ movie-trailer-website/media.py
@@ -1,10 +1,14 @@
 import webbrowser
 
-class Movie:
 
-	# return a Movie object with a csv line
+class Movie():
+	""" Movie class contains information of a movie: title, post and trailer """
+
 	def make_movie(entry):
+		""" return a Movie object with a csv line 
+		"""
 		return Movie(entry[0], entry[1], entry[2])
+	
 	make_movie = staticmethod(make_movie)
 
 	def __init__(self, title, poster_image, trailer_ytb):
@@ -12,9 +16,10 @@ class Movie:
 		self.poster_image_url = poster_image
 		self.trailer_ytb_url = trailer_ytb
 
-	# open youtube video 
 	def show_trailer(self):
+		""" open youtube video """
 		webbrowser.open(self.trailer_ytb_url)
 
 	def __str__(self):
-		return "title: " + self.title + ", image: " + self.poster_image_url + ", trailer: " + self.trailer_ytb_url
+		return "title: " + self.title + ", image: " + \
+            self.poster_image_url + ", trailer: " + self.trailer_ytb_url
